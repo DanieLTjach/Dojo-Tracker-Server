6825f3eda3f3b75b86c5a967e53dc3dff7b98d31
Danylo Tkach <58009634+DanieLTjach@users.noreply.github.com>
2024-12-23 18:12:27 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,23 +4,12 @@ const app = express()
 const PORT = 3000;
 
 const authRoutes = require('./service/auth/authRoutes');
-const gameRoutes = require('./service/game/gameRoutes')
 
 app.use(express.json());
 
 app.use(cors());
 
 app.use('/api/auth', authRoutes);
-app.use('/api/game', gameRoutes);
-// app.post('/get_game', (req, res) =>{
-//     const players = req.body;
-
-//     // Логируем данные на сервере
-//     console.log('Received players data:', players);
-
-//     // Ответ клиенту
-//     res.status(200).json({ message: 'Players data received successfully.', players });
-// })
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://176.37.99.189:${PORT}`);
