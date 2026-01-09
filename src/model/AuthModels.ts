b40a8965fcef896da905ca54bb08ff889c5b278c
Ihor S. <play@shogi-dojo.com>
2026-01-09 13:24:02 +0200

    refactor: simplify TelegramUser to only include id field

--- src/model/AuthModels.ts
+++ src/model/AuthModels.ts
@@ -20,10 +20,4 @@ export interface DecodedToken {
  */
 export interface TelegramUser {
     id: number;
-    first_name?: string;
-    last_name?: string;
-    username?: string;
-    language_code?: string;
-    is_premium?: boolean;
-    photo_url?: string;
 }
