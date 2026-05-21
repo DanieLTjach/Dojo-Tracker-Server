1f68e1cb6331fe2cac1a9bf2740e84ef1cab37de
Viktor Kryshtal <victor.kryshtal@gmail.com>
2026-05-21 23:31:18 +0300

    Updated starting points in EMA + red fives ruleset

--- db/migrations/7.sql
+++ db/migrations/7.sql
@@ -90,4 +90,4 @@ CREATE TABLE gameRound (
 ALTER TABLE event ADD COLUMN info TEXT;
 
 -- set EMA starting points to 30000
-UPDATE gameRules SET startingPoints = 30000 WHERE id = 4;
+UPDATE gameRules SET startingPoints = 30000 WHERE id = 4 OR id = 11;
